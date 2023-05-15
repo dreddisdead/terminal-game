58dca8c42b6298a039812a96bc996bb86636eff1
Cam <126632693+dreddisdead@users.noreply.github.com>
2023-05-15 12:29:08 -0400

    Create user greeting and set up git

---
+++ main.py
@@ -0,0 +1,19 @@
+#First portfolio project
+big_string = """
+  _  _       _  _        _   
+| || | ___ | || | ___  | |  
+| __ |/ -_)| || |/ _ \ |_|  
+|_||_|\___||_||_|\___/ (_)  
+"""
+greeting = 'Welcome to the game!'
+
+print(big_string)
+print(greeting)
+
+user_name = input('What is your name?\n')
+greeting_user = f'Hello, {user_name}'
+
+print(greeting_user)
+
+
+
