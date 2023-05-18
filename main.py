cdc1cfcdb5a8660bb78de787208d1c9d7e50297a
Cam <126632693+dreddisdead@users.noreply.github.com>
2023-05-17 21:56:16 -0400

    Create game intro

--- main.py
+++ main.py
@@ -1,51 +1,79 @@
-#First portfolio project
+### First portfolio project! (super exciting)
+### This adventure terminal game is a little...different.
+### Guess there's only one way to find out why.
+
+import functions as f
+import os, sys
+import time
+import keyboard 
+
+def type_text(text):
+    for char in text:
+        print(char, end='', flush=True)
+        sys.stdout.flush()
+        time.sleep(0.07)  # Adjust the delay time as needed
+    print()  # Print a newline after the typed text
+
+def clear_screen(): 
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def print_ascii_art(ascii_art):
+    print(ascii_art)
+
+def transition_to_scene(scene_ascii_art):
+    clear_screen()
+    print_ascii_art(scene_ascii_art)
+    time.sleep(2)  # Adjust the delay time as needed
 
 
-big_string = """
-  _  _       _  _        _   
-| || | ___ | || | ___  | |  
-| __ |/ -_)| || |/ _ \ |_|  
-|_||_|\___||_||_|\___/ (_)  
+
+### screen size is 96x46
+big_string = """                                                                                        
+-----------------------------------------------------------------------------------------------                                                                                          
+                                           -                                              
+                                           *:  :*                                         
+                                          **:  -*+                                        
+                                          **-  -*+                                        
+                                :.   -:   =*-  =*=   :       :.                           
+                                :+   -*:  -*=  =*:  :=     :+=                            
+                   :=.         .**=  -**  .*=  +*.  +**   -**.         :-++               
+              -*-   .+*+.   :   .**:  +*:  *+  +*  .**:  -*+.   :-   :+*+:                
+               -**-   :+*-  -*=  .**  .*+  ++  *+  =*=  :*=   =*=  .=*+:   ::             
+                .=**-   -*+. :*+. :*+  =*. -*  *- .*+  :*=  .++:  -*+:   -+-              
+                   -**-   =*-  +*. -*- .*- .*  *: =*: :*-  :*=  :*+:  .-**=               
+                 :.  :+*-  :++. -*: -*: -*  *..*. *- .*-  =+. .++:  :=*=:                 
+                  ++:  .=*-  :+- .+- =*  +: +..* -+ .+: .+-  =+:  :++-.  .                
+           -=:     :=*=.  -+-  -+. =- =+ .: :..- -  +: -+. :+: .-*=.  :++.                
+            .=**+-    :++-  :+- .=- ::.=-=+*****++=-: =- .=: .=+:  :=*=-    :.            
+               :-+*+-.   :+=: .=- .--+****************=.-: :=-. :=+-:  .:=+**=--          
+                   .-=+=-.  -=-. :-*********************+.-. :==:  .-=**+-:.              
+           .-.=*+=-.   .:=+=: .::+************************-.-..:-++=-:.    .--:-.         
+             .:--=+***=-:.  :--.***************************-.==-.   .:-=+***+=:.          
+       -:::...       ..:-===-. +****************************: :-====--::.         ...     
+    :==++++++++**++++===---::.:*****************************+ .  ...::---===++*******+--. 
+                     ....:::-:-****************************** ---:::::::.......           
+----------------------------------------------------------------------------------------------- 
+        WELCOME   TO   THE    GAME   ISN'T   THIS   SUNRISE   JUST   BEAUTIFUL!!!!        
+-----------------------------------------------------------------------------------------------
+
 """
-greeting = 'Welcome to the game!'
+greeting = "You've got a long road ahead of you, traveler."
 
-print(big_string)
+print_ascii_art(big_string)
 
-print(greeting)
+type_text(greeting) 
 
-user_name = input('What is your name?\n')
+time.sleep(2)
+
+user_name = input('What is your name?\n>')
 greeting_user = f'Hello, {user_name}'
 
 print(greeting_user)
 
-user_birthday = input('What is your date of birth (mm/dd):\n')
+#user_birthday = input('What is your date of birth (mm/dd):\n')
 
-def zodiac_sign(birthday):
-    bday_list = birthday.split('/')
-    month = int(bday_list[0])
-    day = int(bday_list[1])
-    # Define the start and end dates for each zodiac sign
-    zodiac_dates = {
-        'Aries': [(3, 21), (4, 19)],
-        'Taurus': [(4, 20), (5, 20)],
-        'Gemini': [(5, 21), (6, 20)],
-        'Cancer': [(6, 21), (7, 22)],
-        'Leo': [(7, 23), (8, 22)],
-        'Virgo': [(8, 23), (9, 22)],
-        'Libra': [(9, 23), (10, 22)],
-        'Scorpio': [(10, 23), (11, 21)],
-        'Sagittarius': [(11, 22), (12, 21)],
-        'Capricorn': [(12, 22), (12, 31)],
-        'Aquarius': [(1, 20), (2, 18)],
-        'Pisces': [(2, 19), (3, 20)]
-    }
-    for sign, (start_date, end_date) in zodiac_dates.items():
-        if (month, day) >= start_date and (month, day) <= end_date:
-            return sign
 
-    return "Unknown"
-
-print(f"You're a {zodiac_sign(user_birthday)}")
+#print(f"You're a {f.zodiac_sign(user_birthday)}")
 
 
 
