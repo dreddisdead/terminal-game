36739d964ef4b8a7909f8e0a114fdcfcce9c8d29
Cam <126632693+dreddisdead@users.noreply.github.com>
2023-05-31 22:20:13 -0400

    Completely refactored code
    Deleted old code and built from scratch with curses to build te game on top of. Game now has a functional intro screen, Main Menu, and Pause Menu.
    
    Narrated and Thought text is typed across the screen for a visual appeal.

--- main.py
+++ main.py
@@ -1,170 +1,225 @@
-
-import scenes as s
-import functions as f
-import os, sys
-import time
-import keyboard 
-import os
-### finish learning curses to fully transform terminal game
 import curses
+import sys
+import time
+
 from curses import wrapper
+from curses.textpad import Textbox, rectangle
+from enum import Enum
+
+# Define an enumeration for menu options
+class MenuOptions(Enum):
+    PLAY = "Play"
+    EXIT = "Exit"
 
 
-def main(stdscr):
-    stdscr.clear()
-    stdscr.addstr(10, 10, "Hello World", curses.A_UNDERLINE)
-    stdscr.refresh()
-    stdscr.getch()
+class ExitChoices(Enum):
+    YES = "Yes"
+    NO = "No"
+
+
+class PauseChoices(Enum):
+    RESUME = "Resume"
+    OPTIONS = "Options"
+    QUIT = "Quit to Main Menu"
+
+
+class Menu:
+    def __init__(self, stdscr, options, title):
+        self.title = title
+        self.stdscr = stdscr
+        self.options = options
+        self.current_row_idx = 0
+
+    def center_text(self, text):
+        h, w = self.stdscr.getmaxyx()
+        x = w // 2 - len(text) // 2
+        y = h // 2 - 4
+        self.stdscr.addstr(y, x, text)
     
+    def print_menu(self):
+        self.stdscr.clear()
+        self.center_text(self.title)
+        h, w = self.stdscr.getmaxyx()
+        for idx, option in enumerate(self.options):
+            x = w//2 - len(option.value)//2
+            y = h//2 - 2 + idx
+            if idx == self.current_row_idx:
+                self.stdscr.attron(curses.color_pair(1))
+                self.stdscr.addstr(y, x, option.value)
+                self.stdscr.attroff(curses.color_pair(1))
+            else:
+                self.stdscr.addstr(y, x, option.value)
+        self.stdscr.refresh()
+
+    def navigate(self):
+        while True:
+            key = self.stdscr.getch()
+            if key == curses.KEY_UP and self.current_row_idx > 0:
+                self.current_row_idx -= 1
+            elif key == curses.KEY_DOWN and self.current_row_idx < len(self.options) - 1:
+                self.current_row_idx += 1
+            elif key in [curses.KEY_ENTER, 10, 13]:
+                return self.options[self.current_row_idx]
+            self.print_menu()
+
+
+class GameScenes:
+    def __init__(self, stdscr):
+        self.stdscr = stdscr
+        
+    def wrap_text(self, text, width):
+        words = text.split(' ')
+        lines = []
+        current_line = []
+
+        for word in words:
+            # If adding a new word doesn't push the length
+            # over the window's width, add the word to the current line
+            if len(' '.join(current_line + [word])) < width:
+                current_line.append(word)
+            else:
+                # Otherwise, add the current line to the list of lines
+                # and start a new line with the current word
+                lines.append(' '.join(current_line))
+                current_line = [word]
+
+        # Add the last line
+        lines.append(' '.join(current_line))
+
+        return lines
+
+  
+    def typed_text(self, text):
+        h, w = self.stdscr.getmaxyx()
+        y = h // 2
+        lines = self.wrap_text(text, w)
+
+        for line in lines:
+            x = w // 2 - len(line) // 2
+            if x < w and y < h:  # Ensure x and y are within the window's boundaries
+                for char in line:
+                    self.stdscr.addstr(y, x, char)
+                    self.stdscr.refresh()
+                    time.sleep(0.01)
+                    x += 1
+                y += 1
+
+        self.stdscr.getch()
+
+        
+
+    def center_text(self, text):
+        h, w = self.stdscr.getmaxyx()
+        y = h // 2
+        lines = self.wrap_text(text, w)
+
+        for line in lines:
+            x = w // 2 - len(line) // 2
+            if x < w and y < h:  # Ensure x and y are within the window's boundaries
+                self.stdscr.addstr(y, x, line)
+                y += 1
+
+    def intro(self):
+        self.stdscr.nodelay(True)  # Make getch non-blocking
+
+        # Display the intro sequence
+        for title in ["Welcome to the game!", "This is the intro!", "What is your name?"]:
+            self.stdscr.clear()
+            self.center_text(title)
+            self.stdscr.refresh()
+            time.sleep(1)
+
+        # Ask for the user's name
+        height, width = 1, 18
+        start_y, start_x = self.stdscr.getmaxyx()
+        start_y = start_y // 2 + 2
+        win = curses.newwin(height, width, start_y, (start_x - width) // 2)
+        rectangle(self.stdscr, start_y - 1, (start_x - width) // 2 - 1, start_y + height, (start_x + width) // 2)
+        self.stdscr.refresh()
+        win.keypad(True)  # Allow special keys to be recognized
+
+        box = Textbox(win, insert_mode=True)
+
+        while True:
+            box.edit()
+            user_name = box.gather().strip()
+
+            if len(user_name) <= width:
+                break
+            else:
+                win.clear()
+                win.refresh()
+
+        greeting = "We're excited to have you, " + user_name + "!"
+        self.stdscr.clear()
+        self.center_text(greeting)
+        self.stdscr.refresh()
+        time.sleep(2)
+
+        self.stdscr.nodelay(False)  # Return getch to blocking mode
+        
+    def bedroom(self):
+        # No delay on getch
+        self.stdscr.nodelay(True)
+        # Display narration sequence
+        for narration in ["...you're sleeping soundly...", "...dreaming peacefully...", "...the land of dreams...", "...where polarities dissolve into one...", "...one true form...", "...emptiness..."]:
+            self.stdscr.clear()
+            self.typed_text(narration)
+            time.sleep(1)
+        # Display thought sequence
+        thought_chunk_one = "You wake up to the sun beaming through your eyelids, causing your eyes to be flooded with a bright red light as you squint for dear life."
+        thought_chunk_two = "Thankfully, your vision was not permanently damaged!"
+        thought_chunk_three = "You find yourself in your bedroom, sitting at the edge of your bed. You need to find your glasses if you want to leave this room without injuring yourself."
+        thought_chunk_four = "Seriously, you can't see anything without them."
+        for observed_thought in [thought_chunk_one, thought_chunk_two, thought_chunk_three, thought_chunk_four]:
+            self.stdscr.clear()
+            self.typed_text(observed_thought)
+            time.sleep(1)
+        # Reset getch to blocking mode
+        self.stdscr.nodelay(False)
+  
+class Game:
+    def __init__(self, stdscr):
+        self.stdscr = stdscr
+        self.main_menu = Menu(stdscr, [MenuOptions.PLAY, MenuOptions.EXIT], "Main Menu")
+        self.pause_menu = Menu(stdscr, [PauseChoices.RESUME, PauseChoices.OPTIONS, PauseChoices.QUIT], "Paused")
+        self.exit_menu = Menu(stdscr, [ExitChoices.YES, ExitChoices.NO], "Are you sure you want to exit?")
+        self.scenes = GameScenes(stdscr)
+
+    def game_loop(self):
+        curses.curs_set(0)
+        curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+        self.scenes.intro()
+        self.main_menu.print_menu()
+        while True:
+            chosen_option = self.main_menu.navigate()
+            if chosen_option == MenuOptions.PLAY:
+                self.play_game()
+            elif chosen_option == MenuOptions.EXIT:
+                self.exit_menu.print_menu()
+                chosen_exit_option = self.exit_menu.navigate()
+                if chosen_exit_option == ExitChoices.YES:
+                    sys.exit()
+                elif chosen_exit_option == ExitChoices.NO:
+                    self.main_menu.print_menu()
+
+    def play_game(self):
+        while True:
+            # insert the actual game logic here
+            self.scenes.bedroom()
+            key = self.stdscr.getch()
+            if key == ord('p') or key == ord('P'):
+                self.pause_menu.print_menu()
+                chosen_pause_option = self.pause_menu.navigate()
+                if chosen_pause_option == PauseChoices.QUIT:
+                    break
+                
+        self.main_menu.print_menu()
+def main(stdscr):
+    game = Game(stdscr)
+    game.game_loop()
+
+
+# Run the main function
 wrapper(main)
-
-
-def set_terminal_size(width, height):
-    os.system(f'printf "\\e[8;{height};{width}t"')
-
-# Set terminal size to 96 columns and 46 rows
-set_terminal_size(96, 46)
-
-def type_text(text, delay=0.05):
-    for char in text:
-        sys.stdout.write(char)
-        sys.stdout.flush()
-        time.sleep(delay)
-    sys.stdout.write('\n')
-    sys.stdout.flush()
-
-### Checks if being run on non-Windows system (Git Bash) and clears terminal
-def clear_screen():
-    os.system('cls')
-
-def print_ascii_art(ascii_art):
-    print(ascii_art)
-
-def transition_to_scene(scene_ascii_art):
-    clear_screen()
-    print_ascii_art(scene_ascii_art)
-    time.sleep(2)  # Adjust the delay time as needed
-
-
-### putting this away for now
-
-for_now = """user_name = input('What is your name?\n>')
-
-
-greeting = f"A grand adventure awaits you, {user_name}.\n" 
-
-print_ascii_art(s.sunrise_scene)
-
-type_text(greeting)
-type_text("Wake up.")
-
-time.sleep(2)
-
-clear_screen()
-
-
-
-
-
-transition_to_scene(s.mailbox_scene)
-
-#user_birthday = input('What is your date of birth (mm/dd):\n')
-
-#print(f"You're a {f.zodiac_sign(user_birthday)}")
-look_counter = 0
-while True:
-    action = input('(Look/Open)\n')
-
-    if action == 'Look':
-        # Look at mailbox
-        if look_counter >= 2:
-            type_text('You really enjoy looking at this mailbox.')
-            time.sleep(1)
-            type_text('As long as you are happy, I guess...')
-            time.sleep(2)
-            continue
-        else:
-            type_text('It is, in fact, a mailbox.')
-            look_counter += 1
-            continue  # Continue to prompt for input again
-
-    elif action == 'Open':
-        # Open mail
-        type_text('You open the mail.')
-        type_text('It is mostly spam and ads.')
-        time.sleep(1)
-        type_text('Oh wait.')
-        time.sleep(1)
-        type_text('There is a letter for you.')
-        time.sleep(1)
-        type_text('You open it.')
-        break  # Exit the loop after the "Open" action
-
-    else:
-        type_text('Invalid action. Please try again.')
-
-amended_letter = f"{s.letter_top}\n Dear {user_name}" + s.letter_from_anna
-
-transition_to_scene(amended_letter)
-
-input("Press Enter to Continue")
-
-cafe = "el cafe"
-
-transition_to_scene(cafe)
-
-type_text('A few days later...', 0.08)
-
-type_text('You find yourself in line to order at a local cafe. Quite a nice place. ')
-time.sleep(1)
-type_text('To be honest')
-time.sleep(2)
-type_text("You're not entirely sure who you're looking for...")
-time.sleep(0.5)
-type_text("You're hoping they recognize you first.")
-time.sleep(1)
-
-# look counter resets to zero
-look_counter = 0
-while True:
-    action = input('(Look/Order)\n')
-
-    if action == 'Look':
-        # Look around in cafe
-        if look_counter >= 2:
-            type_text("Yes, yes...")
-            time.sleep(1)
-            type_text('A very nice cafe, indeed.')
-            time.sleep(2)
-            continue
-        else:
-            type_text("You look around at the beautiful art on nearly every wall.")
-            type_text("This place radiates passion and comfort elegantly.")
-
-            look_counter += 1
-            continue  # Continue to prompt for input again
-
-    elif action == 'Order':
-        # Make an order
-        type_text("You walk up to the counter and the employee greets you.\n")
-        time.sleep(1)
-        type_text("Hello, Welcome to Abrianna's Cafe and Bakery!")
-        type_text("Home to tasty homemade sweets and heartwarming food.")
-        time.sleep(1)
-        type_text("What would you like to order?")
-        # Show featured items
-        time.sleep(1)
-        print(s.menu)
-        menu_items = []
-        menu_choice = input("Order Here:\n")
-        break  # Exit the loop after the "Open" action
-
-    else:
-        type_text('Invalid action. Please try again.')"""
-
-
-
-
-
-
-
+ 
